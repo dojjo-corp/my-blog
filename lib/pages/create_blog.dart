e935b6eeee9d52e36df1020ba2b3184f9061ee22
dojjo-desktop <hosuatetteh@gmail.com>
2023-07-06 01:34:54 +0000

    new page

---
+++ lib/pages/create_blog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CreateBlog extends StatelessWidget {
+  const CreateBlog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Create Your Blog')),
+        body: const Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
