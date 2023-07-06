99d5f420a505785bec8612bdd0d817764101fffd
dojjo-desktop <hosuatetteh@gmail.com>
2023-07-06 01:34:28 +0000

    dummy blog page

--- lib/pages/blogs.dart
+++ lib/pages/blogs.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:dojjoblog/pages/dashboard.dart';
 
 class BlogDetails extends StatelessWidget {
-  const BlogDetails({super.key});
+  const BlogDetails({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +17,65 @@ class BlogDetails extends StatelessWidget {
           title: const Text('Blog'),
         ),
         drawer: const MyDrawer(),
-        body: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // todo: actual blog data will be from JSON files
+                const Row(
+                  children: [
+                    Text(
+                      'Blog Topic',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueGrey),
+                    ),
+                  ],
+                ),
+                Image.asset('assets/influencer.png'),
+                const Row(
+                  children: [
+                    Text(
+                      'Date',
+                      style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                    ),
+                    VerticalDivider(),
+                    Text(
+                      'Time',
+                      style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                    ),
+                    VerticalDivider(
+                      thickness: 16,
+                      width: 50,
+                    ),
+                    Text(
+                      'Author',
+                      style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0,),
+                const Text(
+                  '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.''',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 20,),
+                const Row(
+                  children: [
+                    Icon(Icons.add_comment_rounded),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-        
       ),
     );
   }
