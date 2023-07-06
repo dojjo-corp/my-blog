59bb462b1d5f00eeb1162232884e0aa9f4fec285
dojjo-desktop <hosuatetteh@gmail.com>
2023-07-06 01:33:30 +0000

    minor edit

--- lib/components/blog_card.dart
+++ lib/components/blog_card.dart
@@ -11,38 +11,62 @@ class BlogCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            imgPath,
-            fit: BoxFit.cover,
-          ),
-          const Text(
-            "Blog Heading",
-            style: TextStyle(fontSize: 12, color: Colors.blueGrey),
-          ),
-          const Text(
-            "Blog Sub Heading",
-            style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueGrey),
-          ),
-          const Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            style: TextStyle(fontSize: 14,),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext context) => const BlogDetails()));
-            },
-            child: const Text("Read more"),
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              imgPath,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              children: [
+                Text(
+                  "Date",
+                  style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                ),
+                VerticalDivider(
+                  thickness: 10,
+                  color: Colors.black,
+                  width: 16,
+                ),
+                Text(
+                  "Time",
+                  style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10,),
+            const Text(
+              "Blog Topic",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey),
+            ),
+            const Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => const BlogDetails()));
+              },
+              child: const Text("Read more"),
+            )
+          ],
+        ),
       ),
     );
   }
